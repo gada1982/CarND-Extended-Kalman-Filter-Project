75a022423e6d272a1116db9bed88de80e61a32b8
gada1982 <daniel@gattringer.biz>
2017-05-18 08:49:07 +0200

    Add OpenSSL path

--- install-mac.sh
+++ install-mac.sh
@@ -4,7 +4,9 @@ cd uWebSockets
 git checkout e94b6e1
 patch CMakeLists.txt < ../cmakepatch.txt
 mkdir build
-export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig 
+export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig
+export OPENSSL_ROOT_DIR=/usr/local/opt/openssl
+export OPENSSL_LIBRARIES=/usr/local/opt/openssl/lib
 cd build
 cmake ..
 make 
